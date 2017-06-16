74af73081f0f189ff7c76cfc6d175b012b275ca5
RollyAngell <angel.uni18021@gmail.com>
2017-06-16 01:04:33 -0500

    Mi resolucion del level_1

--- Level_1/Level_1.0.py
+++ Level_1/Level_1.0.py
@@ -1,17 +1,19 @@
 import string
 
-alphabet = string.ascii_lowercase
+abecedario=string.ascii_lowercase
 
-def shift(text, offset):
-    shifted = []
-    for letter in text:
-        if letter not in " .'()":
-            i = (alphabet.index(letter) + offset) % len(alphabet)            
-            shifted.append(alphabet[i])
+def contrase(text, offset):
+    hack=[]
+    for i in text:
+        if i not in " .'()":
+            a=(abecedario.index(i)+ offset) % len(abecedario)
+            hack.append(abecedario[a])
         else:
-            shifted.append(letter)
-    return ''.join(shifted)
+            hack.append(i)
+    return ''.join(hack)
 
 text = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 
-print(shift(text, offset=2))  
+print(contrase(text, offset=2))
+
+            
