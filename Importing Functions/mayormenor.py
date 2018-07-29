66c2df1948797814e369dd72fe0471ad3dde9707
RollyAngell07 <roly.inga@pucp.edu.pe>
2018-07-28 22:32:30 -0500

    Import de funciones

---
+++ Importing Functions/mayormenor.py
@@ -0,0 +1,11 @@
+def numero_mayor(X1, X2):
+    if X1 > X2:
+        return X1
+    else:
+        return X2
+    
+def numero_menor(X1, X2):
+    if X1 < X2:
+        return X1
+    else:
+        return X2
